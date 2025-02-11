dd03c6ff22ed4ee68d38b894eccec2e2ecdd7e58
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-02-11 13:47:52 +0100

    Update client

--- Chateq.Client/src/App.jsx
+++ Chateq.Client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 import {
   BrowserRouter as Router,
   Route,
@@ -7,9 +8,14 @@ import {
 } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import { Col, Container, Row } from "react-bootstrap";
+import Chat from "./pages/Chat";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [connection, setConnection] = useState(null);
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const expiryDate = localStorage.getItem("expiryDate");
@@ -18,10 +24,134 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    if (isLoggedIn && !connection) {
+      const token = localStorage.getItem("token");
+      const connect = new HubConnectionBuilder()
+        .withUrl(`http://localhost:5201/messageHub?token=${token}`, {
+          accessTokenFactory: () => token,
+        })
+        .withAutomaticReconnect()
+        .build();
+
+      setConnection(connect);
+
+      return () => {
+        if (connection) {
+          connection.stop();
+        }
+      };
+    }
+  }, [isLoggedIn, connection]);
+
+  useEffect(() => {
+    const fetchInitialMessages = async () => {
+      try {
+        if (isLoggedIn) {
+          const token = localStorage.getItem("token");
+          const response = await fetch(
+            "http://localhost:5201/api/Chat/GetPaginatedChat?chatName=Global&pageNumber=1&pageSize=20",
+            {
+              method: "POST",
+              headers: {
+                Accept: "*/*",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+
+          if (response.ok) {
+            const data = await response.json();
+            const formattedMessages = data.messages.map((msg) => ({
+              sender: msg.sender,
+              message: msg.messageText,
+              createdAt: msg.createdAt,
+            }));
+
+            setSelectedChat({ id: data.id, name: data.name });
+            setMessages(formattedMessages);
+          } else {
+            console.error(
+              "Error while fetching messages from API:",
+              response.status
+            );
+          }
+        }
+      } catch (error) {
+        console.error("Error while connecting to API:", error);
+      }
+    };
+
+    fetchInitialMessages();
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    if (connection) {
+      connection
+        .start()
+        .then(async () => {
+          console.log("Connected with SignalR.");
+          connection.on("ReceiveMessage", (sender, message) => {
+            setMessages((messages) => [...messages, { sender, message }]);
+          });
+        })
+        .catch((error) => console.error("SignalR error:", error));
+    }
+  }, [connection]);
+
   const handleLogin = (authData) => {
     localStorage.setItem("token", authData.token);
     localStorage.setItem("expiryDate", authData.expiryDate);
     setIsLoggedIn(true);
+  };
+
+  const onSendMessage = async (chatId, message) => {
+    if (connection) {
+      try {
+        await connection.invoke("SendMessageToChatAsync", chatId, message);
+      } catch (error) {
+        console.error("Could not send message:", error);
+      }
+    }
+  };
+
+  const onLoadOlderMessages = async (chatName, pageNumber) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        `http://localhost:5201/api/Chat/GetPagnatedChat?chatName=${chatName}&pageNumber=${pageNumber}&pageSize=20`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "*/*",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        const newMessages = data.messages.map((msg) => ({
+          sender: msg.sender,
+          message: msg.messageText,
+          createdAt: msg.createdAt,
+        }));
+
+        if (newMessages.length > 0) {
+          setMessages((prevMessages) => [...newMessages, ...prevMessages]);
+        }
+
+        return newMessages.length === 20;
+      } else {
+        console.error("Error while loading older messages:", response.status);
+        return false;
+      }
+    } catch (error) {
+      console.error("Error while connecting to API:", error);
+      return false;
+    }
   };
 
   return (
@@ -40,6 +170,27 @@ function App() {
         <Route
           path="/register"
           element={isLoggedIn ? <Navigate to="/chat" /> : <Register />}
+        />
+        <Route
+          path="/chat"
+          element={
+            isLoggedIn ? (
+              <Container className="mt-4">
+                <Row>
+                  <Col md={12}>
+                    <Chat
+                      messages={messages}
+                      onSendMessage={onSendMessage}
+                      currentChat={selectedChat}
+                      onLoadOlderMessages={onLoadOlderMessages}
+                    />
+                  </Col>
+                </Row>
+              </Container>
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
         />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
---
+++ Chateq.Client/src/pages/Chat.jsx
@@ -0,0 +1,150 @@
+import { useEffect, useRef, useState } from "react";
+import {
+  Button,
+  Container,
+  Form,
+  InputGroup,
+  ListGroup,
+} from "react-bootstrap";
+
+// eslint-disable-next-line react/prop-types
+function Chat({ messages, onSendMessage, currentChat, onLoadOlderMessages }) {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState(
+    currentChat || { id: "Global", name: "Global" }
+  );
+  const [page, setPage] = useState(1);
+  const [hasInitialLoad, setHasInitialLoad] = useState(false);
+  const messagesEndRef = useRef(null);
+  const chatMessagesRef = useRef(null);
+
+  useEffect(() => {
+    setChat(currentChat);
+  }, [currentChat]);
+
+  useEffect(() => {
+    // eslint-disable-next-line react/prop-types
+    if (!hasInitialLoad && messages.length > 0) {
+      scrollToBottom();
+      setHasInitialLoad(true);
+    }
+  }, [messages, hasInitialLoad]);
+
+  useEffect(() => {
+    if (hasInitialLoad && isScrolledToBottom()) {
+      scrollToBottom();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [messages]);
+
+  useEffect(() => {
+    const chatContainer = chatMessagesRef.current;
+
+    if (chatContainer) {
+      chatContainer.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (chatContainer) {
+        chatContainer.addEventListener("scroll", handleScroll);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chatMessagesRef, page]);
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const isScrolledToBottom = () => {
+    const chatContainer = chatMessagesRef.current;
+    return (
+      chatContainer &&
+      chatContainer.scrollHeight - chatContainer.scrollTop ===
+        chatContainer.clientHeight + 50
+    );
+  };
+
+  const handleScroll = () => {
+    if (chatMessagesRef.current.scrollTop === 0) {
+      fetchOlderMessages();
+    }
+  };
+
+  const fetchOlderMessages = async () => {
+    const newPage = page + 1;
+    const success = await onLoadOlderMessages(chat.id, newPage);
+
+    if (success) {
+      setPage(newPage);
+    }
+  };
+
+  const handleSendMessage = () => {
+    scrollToBottom();
+
+    if (!chat || !chat.id) {
+      console.error("No chat selected or chat's id is missing");
+      return;
+    }
+
+    if (message.trim()) {
+      onSendMessage(chat.id, message);
+      setMessage("");
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSendMessage();
+    }
+  };
+
+  return (
+    <Container className="d-flex flex-column align-items-center vh-100">
+      <div className="w-100" style={{ maxWidth: "600px" }}>
+        <div className="text-center">
+          <h4>{chat?.name} Chat</h4>
+        </div>
+
+        <div
+          ref={chatMessagesRef}
+          style={{ overflowY: "auto", maxHeight: "70vh" }}
+        >
+          <ListGroup variant="flush">
+            {messages
+              // eslint-disable-next-line react/prop-types
+              .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+              .map((msg, index) => (
+                <ListGroup.Item key={index}>
+                  <strong>
+                    {msg.sender}: {msg.message}
+                  </strong>
+                </ListGroup.Item>
+              ))}
+            <div ref={messagesEndRef}></div>
+          </ListGroup>
+        </div>
+
+        <div className="mt-3">
+          <InputGroup>
+            <Form.Control
+              type="text"
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+              onKeyPress={handleKeyPress}
+              placeholder="Enter your message..."
+            />
+            <Button variant="primary" onClick={handleSendMessage}>
+              Send
+            </Button>
+          </InputGroup>
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+export default Chat;
