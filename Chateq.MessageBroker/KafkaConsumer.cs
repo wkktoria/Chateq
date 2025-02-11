b1cb6912f26aad27420cbf1fc4151207daab8990
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-02-11 20:05:30 +0100

    Create message broker

---
+++ Chateq.MessageBroker/KafkaConsumer.cs
@@ -0,0 +1,9 @@
+namespace Chateq.MessageBroker;
+
+public class KafkaConsumer : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Chateq.MessageBroker/Program.cs
@@ -0,0 +1,18 @@
+using Chateq.Core.Domain;
+using Chateq.Core.Domain.Options;
+using Chateq.MessageBroker;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.Configure<KafkaOption>(options
+    => configuration.GetSection(nameof(KafkaOption)).Bind(options));
+
+var connectionString = configuration.GetValue<string>("ConnectionString");
+builder.Services.AddDbContext<ChateqDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddHostedService<KafkaConsumer>();
+
+var app = builder.Build();
+app.Run();
